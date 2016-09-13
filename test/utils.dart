25b71abac01314366129b33e98d69509eb2e5672
Jay.Udey <jay.udey@webfilings.com>
2016-09-13 16:32:44 -0500

    add polling for troublesome test

--- test/utils.dart
+++ test/utils.dart
@@ -5,3 +5,15 @@ import 'dart:async';
 Future nextTick([int milliseconds = 1]) {
   return new Future.delayed(new Duration(milliseconds: milliseconds));
 }
+
+Future waitFor(bool condition(),
+    {Duration interval: const Duration(milliseconds: 1)}) {
+  Completer c = new Completer();
+  new Timer.periodic(interval, (timer) {
+    if (condition()) {
+      timer.cancel();
+      c.complete();
+    }
+  });
+  return c.future;
+}
