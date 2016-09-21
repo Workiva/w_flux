c3161d4b123313f399cc9b7bda5712e67d38080c
Jay.Udey <jay.udey@webfilings.com>
2016-09-21 10:06:26 -0500

    add in timeout feature to waitFor

--- test/utils.dart
+++ test/utils.dart
@@ -7,12 +7,19 @@ Future nextTick([int milliseconds = 1]) {
 }
 
 Future waitFor(bool condition(),
-    {Duration interval: const Duration(milliseconds: 1)}) {
+    {Duration interval: const Duration(milliseconds: 1),
+    int maximumTimeout: 10}) {
   Completer c = new Completer();
+  Stopwatch w = new Stopwatch()..start();
   new Timer.periodic(interval, (timer) {
-    if (condition()) {
+    if (condition() || w.elapsedMilliseconds > maximumTimeout) {
       timer.cancel();
       c.complete();
+      w.stop();
+      if (w.elapsedMilliseconds > maximumTimeout) {
+        throw new TimeoutException(
+            'The expected condition did not occur within $maximumTimeout milliseconds');
+      }
     }
   });
   return c.future;
