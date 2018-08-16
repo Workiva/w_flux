cfe1232058817df9207e60026d79e40508546652
Max Peterson <maxwell.peterson@webfilings.com>
2018-08-16 13:56:02 -0600

    more debugging

--- lib/src/mixins/batched_redraws.dart
+++ lib/src/mixins/batched_redraws.dart
@@ -6,8 +6,10 @@ import 'dart:html';
 import 'package:react/react.dart' as react;
 
 class _RedrawScheduler implements Function {
+  Completer<Null> _completer;
   Map<BatchedRedraws, List<Function>> _components =
       <BatchedRedraws, List<Function>>{};
+  StreamSubscription<Event> _documentVisibilitySubscription;
 
   void call(BatchedRedraws component, [callback()]) {
     if (_components.isEmpty) {
@@ -20,6 +22,15 @@ class _RedrawScheduler implements Function {
   }
 
   Future _tick() async {
+    if (_documentVisibilitySubscription == null) {
+      _documentVisibilitySubscription = document.onVisibilityChange.listen((_) {
+        if (document.hidden && _completer != null && !_completer.isCompleted) {
+          print('    end waiting because document is no longer visible.');
+          _completer.complete();
+          _documentVisibilitySubscription.cancel();
+        }
+      });
+    }
     // `requestAnimationFrame()` (called by [window.animationFrame]) does not
     // fire (or fires with a significant delay depending on the browser) when
     // the current tab/window is not focused.
@@ -29,7 +40,14 @@ class _RedrawScheduler implements Function {
     // focused again, do not batch updates if the document is hidden.
     if (!document.hidden) {
       print('waiting for animationFrame...');
-      await window.animationFrame;
+      _completer = new Completer<Null>();
+      window.animationFrame.then((_) {
+        if (!_completer.isCompleted) {
+          _completer.complete();
+        }
+      });
+
+      await _completer.future;
       print('   done waiting.');
     } else {
       print('Skipping awiat animationFrame, document is hidden!');
