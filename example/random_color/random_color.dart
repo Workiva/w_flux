ef6efdfc6692f36fc8c43eb51dbdefda87343f3b
Aaron Lademann <aaron.lademann@workiva.com>
2019-10-01 08:22:21 -0700

    Run React 16 codemod

--- example/random_color/random_color.dart
+++ example/random_color/random_color.dart
@@ -18,6 +18,7 @@ import 'dart:html';
 import 'dart:math';
 
 import 'package:react/react.dart' as react;
+import 'package:over_react/over_react.dart';
 import 'package:react/react_dom.dart' as react_dom;
 import 'package:react/react_client.dart' as react_client;
 
@@ -30,7 +31,9 @@ main() async {
 
   // render the component
   react_client.setClientConfiguration();
-  react_dom.render(RandomColorComponent({'actions': actions, 'store': store}),
+  react_dom.render(
+      ErrorBoundary()(
+          RandomColorComponent({'actions': actions, 'store': store})),
       querySelector('#content-container'));
 }
 
