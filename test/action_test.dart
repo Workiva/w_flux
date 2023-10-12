02b83a0b8e158c3f03936cf23ca481baff4b0fdb
Greg Littlefield <greg.littlefield@workiva.com>
2023-10-12 16:17:06 -0700

    Add Action2 tests

--- test/action_test.dart
+++ test/action_test.dart
@@ -222,4 +222,193 @@ void main() {
       }, skip: true);
     });
   });
+  
+  group('Action2', () {
+    late Action2<String> action;
+
+    setUp(() {
+      action = Action2<String>();
+    });
+
+    test('should only be equivalent to itself', () {
+      Action2 _action = Action2();
+      Action2 _action2 = Action2();
+      expect(_action == _action, isTrue);
+      expect(_action == _action2, isFalse);
+    });
+
+    test('should support dispatch with a payload', () async {
+      Completer c = Completer();
+      action.listen((payload) {
+        expect(payload, equals('990 guerrero'));
+        c.complete();
+      });
+
+      action('990 guerrero');
+      return c.future;
+    });
+
+    test('should dispatch by default when called', () async {
+      Completer c = Completer();
+      action.listen((payload) {
+        expect(payload, equals('990 guerrero'));
+        c.complete();
+      });
+
+      action('990 guerrero');
+      return c.future;
+    });
+
+    group('dispatch', () {
+      test(
+          'should invoke and complete synchronous listeners in future event in '
+          'event queue', () async {
+        var action = Action2();
+        var listenerCompleted = false;
+        action.listen((_) {
+          listenerCompleted = true;
+        });
+
+        // No immediate invocation.
+        action(null);
+        expect(listenerCompleted, isFalse);
+
+        // Invoked during the next scheduled event in the queue.
+        await Future(() => {});
+        expect(listenerCompleted, isTrue);
+      });
+
+      test(
+          'should invoke asynchronous listeners in future event and complete '
+          'in another future event', () async {
+        var action = Action2();
+        var listenerInvoked = false;
+        var listenerCompleted = false;
+        action.listen((_) async {
+          listenerInvoked = true;
+          await Future(() => listenerCompleted = true);
+        });
+
+        // No immediate invocation.
+        action(null);
+        expect(listenerInvoked, isFalse);
+
+        // Invoked during next scheduled event in the queue.
+        await Future(() => {});
+        expect(listenerInvoked, isTrue);
+        expect(listenerCompleted, isFalse);
+
+        // Completed in next next scheduled event.
+        await Future(() => {});
+        expect(listenerCompleted, isTrue);
+      });
+
+      test('should complete future after listeners complete', () async {
+        var action = Action2();
+        var asyncListenerCompleted = false;
+        action.listen((_) async {
+          await Future.delayed(Duration(milliseconds: 100), () {
+            asyncListenerCompleted = true;
+          });
+        });
+
+        Future<dynamic>? future = action(null);
+        expect(asyncListenerCompleted, isFalse);
+
+        await future;
+        expect(asyncListenerCompleted, isTrue);
+      });
+
+      test('should surface errors in listeners', () {
+        var action = Action2();
+        action.listen((_) => throw UnimplementedError());
+        expect(action(0), throwsUnimplementedError);
+      });
+    });
+
+    group('listen', () {
+      test('should stop listening when subscription is canceled', () async {
+        var action = Action2();
+        var listened = false;
+        var subscription = action.listen((_) => listened = true);
+
+        await action(null);
+        expect(listened, isTrue);
+
+        listened = false;
+        subscription.cancel();
+        await action(null);
+        expect(listened, isFalse);
+      });
+
+      test('should stop listening when listeners are cleared', () async {
+        var action = Action2();
+        var listened = false;
+        action.listen((_) => listened = true);
+
+        await action(null);
+        expect(listened, isTrue);
+
+        listened = false;
+        await action.dispose();
+        await action(null);
+        expect(listened, isFalse);
+      });
+
+      test('should stop listening when actions are disposed', () async {
+        var action = Action2();
+        var listened = false;
+        action.listen((_) => listened = true);
+
+        await action(null);
+        expect(listened, isTrue);
+
+        listened = false;
+        await action.dispose();
+        await action(null);
+        expect(listened, isFalse);
+      });
+    });
+
+    group('benchmarks', () {
+      test('should dispatch actions faster than streams :(', () async {
+        const int sampleSize = 1000;
+        var stopwatch = Stopwatch();
+
+        var awaitableAction = Action2();
+        awaitableAction.listen((_) => {});
+        awaitableAction.listen((_) async {});
+        stopwatch.start();
+        for (var i = 0; i < sampleSize; i++) {
+          await awaitableAction(null);
+        }
+        stopwatch.stop();
+        var averageActionDispatchTime =
+            stopwatch.elapsedMicroseconds / sampleSize / 1000.0;
+
+        stopwatch.reset();
+
+        late Completer syncCompleter;
+        late Completer asyncCompleter;
+        var action = Action2();
+        action.listen((_) => syncCompleter.complete());
+        action.listen((_) async {
+          asyncCompleter.complete();
+        });
+        stopwatch.start();
+        for (var i = 0; i < sampleSize; i++) {
+          syncCompleter = Completer();
+          asyncCompleter = Completer();
+          action(null);
+          await Future.wait([syncCompleter.future, asyncCompleter.future]);
+        }
+        stopwatch.stop();
+        var averageStreamDispatchTime =
+            stopwatch.elapsedMicroseconds / sampleSize / 1000.0;
+
+        print('awaitable action (ms): $averageActionDispatchTime; '
+            'stream-based action (ms): $averageStreamDispatchTime');
+      }, skip: true);
+    });
+  });
 }
